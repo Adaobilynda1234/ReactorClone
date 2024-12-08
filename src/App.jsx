c3c7a9357c09cf97dff607dd7ebb882063835caf
Adaobilynda1234 <adaobilynda1234@gmail.com>
2024-12-09 00:34:33 +0100

    profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
@@ -18,7 +19,7 @@ function App() {
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-          
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </Router>
       <ToastContainer
@@ -37,4 +38,4 @@ function App() {
   );
 }
 
-export default App; 
+export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDedH0_Kd57qI6G2kBMOqDxG69Y9IFlIik",
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "realtorapp-d173e.firebaseapp.com",
   projectId: "realtorapp-d173e",
   storageBucket: "realtorapp-d173e.firebasestorage.app",
   messagingSenderId: "466920490823",
-  appId: "1:466920490823:web:180183dc45255346292e62"
+  appId: "1:466920490823:web:180183dc45255346292e62",
 };
-
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
-export const db = getFirestore()
+export const db = getFirestore();
